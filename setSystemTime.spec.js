af0b5d97c23a1fda19d43351b251dd95772fb6df
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2021-03-27 11:24:47 +0900

    update setSystemTime add comment

--- setSystemTime.spec.js
+++ setSystemTime.spec.js
@@ -1,7 +1,7 @@
 describe(jest.setSystemTime, () => {
     test('can set new Date', () => {
         jest.useFakeTimers('modern')
-        jest.setSystemTime(new Date('2020-02-03'))
+        jest.setSystemTime(new Date('2020-02-03')) //TypeError: setSystemTime is not available when not using modern timers
         const actual = new Date()
         expect(actual.getFullYear()).toBe(2020)
         expect(actual.getMonth()).toBe(1)
